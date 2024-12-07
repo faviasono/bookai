d2658daca9514250264ed5eeabe7d180f59311ca
Andrea Favia <andrea.faviait@gmail.com>
2024-12-07 16:00:19 +0100

    basic tts with elevenlabs

---
+++ tts/tts_elevenlabs.py
@@ -0,0 +1,30 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from models.base_tts import BaseTts
+
+from elevenlabs import ElevenLabs, play
+
+
+# Load the environment variables
+load_dotenv(join(dirname(__file__), ".env"))
+
+
+class ElevenLabsTTS(BaseTts):
+    def __init__(self, model_name="eleven_multilingual_v2", voice_id="21m00Tcm4TlvDq8ikWAM"):
+        self.client = ElevenLabs(api_key=os.environ.get("ELEVEN_API_KEY"))
+        self.model_name = model_name
+        self.voice_id = voice_id
+
+    def synthesize(self, text):
+        return self.client.text_to_speech.convert(
+            voice_id=self.voice_id,
+            model_id=self.model_name,
+            text=text,
+        )
+
+
+if __name__ == "__main__":
+    tts = ElevenLabsTTS()
+    audio = tts.synthesize("Hello, how are you?")
+    play(audio)
