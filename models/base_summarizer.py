2d8c98e87b7f650cbee50bd4f900c5aca88de2c1
Andrea Favia <andrea.faviait@gmail.com>
2024-12-07 17:55:49 +0100

    handle error w specific exception

--- models/base_summarizer.py
+++ models/base_summarizer.py
@@ -1,12 +1,18 @@
+class SummarizationException(Exception):
+    pass
+
 
 class SummarizerBaseModel:
     def summarize(self, text):
         raise NotImplementedError
 
-class Summarizer:
 
+class Summarizer:
     def __init__(self, model: SummarizerBaseModel):
         self.model = model
-    
+
     def summarize(self, text):
-        return self.model.summarize(text)
+        try:
+            return self.model.summarize(text)
+        except Exception as e:
+            raise SummarizationException(f"Error summarizing text: {str(e)}")
