37607b744299351156fb382ba66d1c202a5f4e04
Andrea Favia <andrea.faviait@gmail.com>
2024-12-11 17:02:36 +0100

    input models as classes and not just model names

--- bookai/rag/question_answering_book.py
+++ bookai/rag/question_answering_book.py
@@ -27,11 +27,17 @@ class QuestionAnsweringBook:
     def __init__(self, chapters_dict, embedding_model, gemini_model, gemini_api_key=None):
         self.reader = DictReader(chapters_dict)
         self.documents = self.reader.load_data()
-        self.embedding_model = HuggingFaceEmbedding(model_name=embedding_model)
+        self.embedding_model = (
+            HuggingFaceEmbedding(model_name=embedding_model) if isinstance(embedding_model, str) else embedding_model
+        )
         self.index = VectorStoreIndex.from_documents(self.documents, embed_model=self.embedding_model, show_progress=False)
-        self.llm = Gemini(
-            model=gemini_model,
-            api_key=gemini_api_key if gemini_api_key else os.environ.get("GEMINI_API_KEY"),
+        self.llm = (
+            Gemini(
+                model=gemini_model,
+                api_key=gemini_api_key if gemini_api_key else os.environ.get("GEMINI_API_KEY"),
+            )
+            if isinstance(gemini_model, str)
+            else gemini_model
         )
         self.query_engine = self.index.as_query_engine(llm=self.llm, streaming=True)
 
